98fef7dac917a1bf767c14d7d212b7288482ae79
RomesAll <romesky845@gmail.com>
2026-02-06 19:19:48 +0300

    core(domain layer): добавил сервисы

---
+++ src/core/application/permission_service.py
@@ -0,0 +1,26 @@
+from src.core.domain.domain_services import PermissionRepository
+from src.core.domain.domain_services.permission.dto.permission import GetPermissionDto, PostPermissionDto, UpdatePermissionDto
+
+class PermissionService:
+    def __init__(self, repository: PermissionRepository):
+        self.permission_repository = repository
+
+    def get_permission_by_id(self, permission_id: int) -> GetPermissionDto:
+        permission = self.permission_repository.get_by_id(permission_id)
+        return permission
+
+    def get_all_permission(self) -> list[GetPermissionDto]:
+        permission_list = self.permission_repository.get_all()
+        return permission_list
+
+    def add(self, permission: PostPermissionDto) -> GetPermissionDto:
+        permission = self.permission_repository.add_permission(permission)
+        return permission
+
+    def update(self, permission_id: int, permission: UpdatePermissionDto) -> GetPermissionDto:
+        permission = self.permission_repository.update_permission(permission_id, permission)
+        return permission
+
+    def delete(self, permission_id: int) -> GetPermissionDto:
+        permission = self.permission_repository.delete_permission(permission_id)
+        return permission
